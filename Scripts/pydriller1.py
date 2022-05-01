8451572e5164ed29d801b3b1d925b90dd6dee05d
kpogula <kpogula2@cougarnet.uh.edu>
2022-05-01 16:35:36 -0500

    Pydriller Changes

--- Scripts/pydriller1.py
+++ Scripts/pydriller1.py
@@ -10,35 +10,40 @@ delta_period = 6
 end_date = start_date - relativedelta(months=delta_period) - timedelta(days=-delta_period)
 author_list = {}
 files_list = {}
-added_files_list = {}
-Modified_count = 0
-modified_filenames = []
-for commit in Repository('https://github.com/numpy/numpy.git', since=end_date, to=start_date).traverse_commits():
-    print(commit.committer_date.date().month)
-    for file in commit.modified_files:
-        filename = file.filename
-        if re.match('^.*test.*\.py$', file.filename):
-            if file.change_type == file.change_type.ADD:
-                author_date = commit.author_date
-                print("File {}, Authored_date {}".format(file.filename, commit.author_date))
 
-            if file.change_type == file.change_type.MODIFY:
-                if files_list.get(filename):
-                    files_list[filename] = files_list.get(file.filename) + 1
-                else:
-                    files_list[filename] = 1
-
-                if author_list.get(filename):
-                    author_list.get(file.filename).add(commit.author.name)
-                else:
-                    author_list[filename] = {commit.author.name}
-
-
-
-header = ['name', 'area', 'country_code2', 'country_code3']
-data = ['Afghanistan', 652090, 'AF', 'AFG']
-
-with open('../Outputs/test.csv', 'w') as f:
+with open('../Outputs/FilesAdded.csv', 'w') as f:
+    header = ['File Name', 'Authored Date']
     writer = csv.writer(f)
     writer.writerow(header)
-    writer.writerow(data)
+    for commit in Repository('https://github.com/numpy/numpy.git', since=end_date, to=start_date).traverse_commits():
+        for file in commit.modified_files:
+            filename = file.filename
+            if re.match('^.*test.*\.py$', file.filename):
+                if file.change_type == file.change_type.ADD:
+                    data = [file.filename, commit.author_date]
+                    writer.writerow(data)
+
+                if file.change_type == file.change_type.MODIFY or file.change_type == file.change_type.RENAME:
+                    if files_list.get(filename):
+                        files_list[filename] = files_list.get(file.filename) + 1
+                    else:
+                        files_list[filename] = 1
+
+                    if author_list.get(filename):
+                        author_list.get(file.filename).add(commit.author.name)
+                    else:
+                        author_list[filename] = {commit.author.name}
+
+with open('../Outputs/FilesModified.csv', 'w') as f:
+    w = csv.writer(f)
+    header = ['File Name', 'Modification Count']
+    w.writerow(header)
+    w.writerows(files_list.items())
+
+with open('../Outputs/AuthorList.csv', 'w') as f:
+    w = csv.writer(f)
+    header = ['File Name', 'Authors']
+    w.writerow(header)
+    w.writerows(author_list.items())
+
+
